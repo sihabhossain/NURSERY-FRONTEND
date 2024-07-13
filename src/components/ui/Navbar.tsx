5082dba560fb00c2221a0342c2e8cd8cbd6699a1
Sihab Hossain <sihabhossain380@gmail.com>
2024-07-13 22:30:03 +0600

    frontend done

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { ShoppingCart, Menu, X } from "lucide-react";
 import Logo from "../../../src/assets/logo.png";
 import Container from "./Container";
@@ -8,8 +8,16 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+  useEffect(() => {
+    if (isMenuOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  }, [isMenuOpen]);
+
   return (
-    <nav className="bg-white shadow-md">
+    <nav className="bg-white shadow-md  w-full z-10">
       <Container>
         <div className="container mx-auto px-4 flex justify-between items-center py-4 md:py-8">
           <div className="flex items-center">
@@ -42,50 +50,45 @@ const Navbar = () => {
               <Sidebar />
             </div>
             <button
-              className={`md:hidden transition-transform duration-300 transform ${
-                isMenuOpen ? "rotate-180" : "rotate-0"
-              }`}
+              className="md:hidden"
               onClick={() => setIsMenuOpen(!isMenuOpen)}
             >
               {isMenuOpen ? (
-                <X className="w-6 h-6 transition-transform duration-300 transform hover:rotate-90" />
+                <X className="w-6 h-6" />
               ) : (
-                <Menu className="w-6 h-6 transition-transform duration-300 transform hover:rotate-90" />
+                <Menu className="w-6 h-6" />
               )}
             </button>
           </div>
         </div>
         <div
-          className={`md:hidden px-4 pb-4 space-y-4 text-gray-600 ${
-            isMenuOpen ? "block" : "hidden"
-          }`}
-          style={{
-            transition: "height 0.5s ease-in-out, opacity 0.5s ease-in-out",
-            maxHeight: isMenuOpen ? "1000px" : "0",
-            opacity: isMenuOpen ? "1" : "0",
-            overflow: "hidden",
-          }}
+          className={`md:hidden transition-all duration-500 ease-in-out ${
+            isMenuOpen ? "max-h-screen opacity-100" : "max-h-0 opacity-0"
+          } overflow-hidden`}
         >
-          <Link
-            to={"/"}
-            className="hover:text-green-600 hover:underline hover:underline-offset-4 hover:decoration-green-600"
-          >
-            HOME
-          </Link>
-          <Link
-            to={"/shop"}
-            className="hover:text-green-600 hover:underline hover:underline-offset-4 hover:decoration-green-600"
-          >
-            PRODUCTS
-          </Link>
-          <Link
-            to={"/management"}
-            className="hover:text-green-600 hover:underline hover:underline-offset-4 hover:decoration-green-600"
-          >
-            MANAGEMENT
-          </Link>
-          <div className="flex justify-around space-y-4 items-center">
-            <div className="relative">
+          <div className="px-4 pb-4 space-y-4 text-gray-600">
+            <Link
+              to={"/"}
+              className="block hover:text-green-600 hover:underline hover:underline-offset-4 hover:decoration-green-600"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              HOME
+            </Link>
+            <Link
+              to={"/shop"}
+              className="block hover:text-green-600 hover:underline hover:underline-offset-4 hover:decoration-green-600"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              PRODUCTS
+            </Link>
+            <Link
+              to={"/management"}
+              className="block hover:text-green-600 hover:underline hover:underline-offset-4 hover:decoration-green-600"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              MANAGEMENT
+            </Link>
+            <div className="flex justify-around items-center">
               <ShoppingCart className="w-6 h-6 cursor-pointer hover:text-green-600" />
             </div>
           </div>
--- src/redux/api/api.ts
+++ src/redux/api/api.ts
@@ -2,7 +2,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api/v1" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://nursery-backend-six.vercel.app/api/v1",
+  }),
   tagTypes: ["products", "order"],
   endpoints: (builder) => ({
     getProducts: builder.query({
